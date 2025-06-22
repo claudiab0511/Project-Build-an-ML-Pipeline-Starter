d4abd58a3d8391237212a9031093666f2b444d46
claudiab0511 <117995990+claudiab0511@users.noreply.github.com>
2025-06-22 11:18:13 -0700

    Confirmed geo location cleaning for sample2.csv

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -30,6 +30,7 @@ def go(args):
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+   #geolocation cleaning- ensures rows with invalid latitude/longitude are filtered out
     idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
     df = df[idx].copy()
     # Save the cleaned file
